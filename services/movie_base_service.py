89c44f969aaa6d05d9f998151e64b697c2288cb2
Дмитрий Казаков <dmitryifile@yandex.ru>
2025-09-06 19:14:03 +0300

    Фикс сервиса, чтобы работал и на windows

--- services/movie_base_service.py
+++ services/movie_base_service.py
@@ -1,5 +1,6 @@
 import os
 from typing import List, Tuple
+from pathlib import Path
 
 from config import config
 
@@ -11,10 +12,18 @@ class MovieBaseService:
 
     def get_all_movies(self) -> List[Tuple[str, str]]:
         movies = []
-        for dirpath, _, filenames in os.walk(self.movie_folder):
-            for filename in filenames:
-                if os.path.splitext(filename)[1].lower() in self.extensions:
-                    full_path = os.path.join(dirpath, filename)
-                    movies.append((filename.split(".")[0], full_path))
+        movie_path = Path(self.movie_folder)
+        
+        if not movie_path.exists():
+            return movies
+            
+        for file_path in movie_path.rglob('*'):
+            if file_path.is_file():
+                if file_path.suffix.lower() in self.extensions:
+                    # Используем stem для получения имени файла без расширения
+                    movie_name = file_path.stem
+                    # Преобразуем путь в строку для совместимости
+                    full_path = str(file_path)
+                    movies.append((movie_name, full_path))
         
         return movies
